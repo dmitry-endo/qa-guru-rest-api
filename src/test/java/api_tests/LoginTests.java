0401083d656d848903e04edc88cc554a45348eb3
d.petrov <d.petrov@infosec.ru>
2025-07-30 01:32:26 +0300

    Added lesson 16 homework

---
+++ src/test/java/api_tests/LoginTests.java
@@ -0,0 +1,87 @@
+package api_tests;
+
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import models.AuthRequestModel;
+import models.AuthResponseModel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static specs.AuthSpec.*;
+
+@Tag("login_tests")
+@Owner("dmitry_endo")
+@DisplayName("Test for API login")
+public class LoginTests extends TestBase {
+
+    @Test
+    @DisplayName("Check successful login")
+    void successfulLoginTest() {
+        AuthRequestModel authData = new AuthRequestModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        AuthResponseModel response = step("Make request", () ->
+                given(authRequestSpec)
+                        .body(authData)
+
+                        .when()
+                        .post(LOGIN_PATH)
+
+                        .then()
+                        .spec(authResponseSpec200)
+                        .extract().as(AuthResponseModel.class));
+
+        step("Check response", () -> {
+            assertThat(response.getToken()).isEqualTo("QpwL5tke4Pnpja7X4");
+        });
+    }
+
+    @Test
+    @DisplayName("Check unsuccessful login with missing password")
+    void missingPasswordLoginWithSpecsTest() {
+        AuthRequestModel authData = new AuthRequestModel();
+        authData.setEmail("eve.holt@reqres.in");
+
+        AuthResponseModel response = step("Make request", () ->
+                given(authRequestSpec)
+                        .body(authData)
+
+                        .when()
+                        .post(REGISTER_PATH)
+
+                        .then()
+                        .spec(authResponseSpec400)
+                        .extract().as(AuthResponseModel.class));
+
+        step("Check response", () -> {
+            assertThat(response.getError()).isEqualTo("Missing password");
+        });
+    }
+
+    @Test
+    @DisplayName("Check unsuccessful login with missing email")
+    void missingEmailRegisterWithSpecsTest() {
+        AuthRequestModel authData = new AuthRequestModel();
+        authData.setPassword("pistol");
+
+        AuthResponseModel response = step("Make request", () ->
+                given(authRequestSpec)
+                        .body(authData)
+
+                        .when()
+                        .post(REGISTER_PATH)
+
+                        .then()
+                        .spec(authResponseSpec400)
+                        .extract().as(AuthResponseModel.class));
+
+        step("Check response", () -> {
+            assertThat(response.getError()).isEqualTo("Missing email or username");
+        });
+    }
+}
---
+++ src/test/java/api_tests/TestBase.java
@@ -0,0 +1,18 @@
+package api_tests;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    protected static final String API_KEY = "reqres-free-v1";
+
+    protected static final String REGISTER_PATH = "/register";
+    protected static final String LOGIN_PATH = "/login";
+    protected static final String USERS_PATH = "/users";
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "https://reqres.in/";
+        RestAssured.basePath = "/api";
+    }
+}
